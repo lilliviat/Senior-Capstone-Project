bee327227d47baaddd4f4e861640af84fbeaf507
Liv <ltayl150@students.kennesaw.edu>
2022-04-02 15:34:16 -0400

    Docker not working for MacOS

--- src/main/deepFlowConsumer.java
+++ src/main/deepFlowConsumer.java
@@ -1,8 +1,89 @@
 /*
-*@authors Lillivia T, Nishik P, Alejandro D, Cole H, Gabe M
-*This application consumes messages at one
-*end of a data pipeline.
+*@authors 
+* Lillivia T
+* Nishik P
+* Alejandro D
+* Cole H
+* Gabe M
+*
+*
+* This application consumes messages at one
+* end of a data pipeline.
+* This application will use stream processing, i.e.
+* the visible boundary doesn't exist, for optimal data flow.
+*
+* The data pipeline is often referred to as a Topic in Kafka
+* , and will be referred to as a topic for the remainder of this API.
+*
+*
+* This Consumer API will send the data from the Producer to be
+* stored into S3. When implemented, it will send data
+* from specified data stores into Kafka near-real time.
 */
+
+//Maven Kafka Package
+package org.apache.kafka.clients;
+
+//Java import statements
+import org.apache.kafka.clients.admin;
+import org.apache.kafka.clients.producer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.Collections;
+
 public class deepFlowConsumer {
-    
-}
+
+    private final static String TOPIC = "swedatapipeline";
+    private final static String BOOTSTRAP_SERVERS =
+            "localhost:9092,localhost:9093,localhost:9094";
+
+    private static Consumer<Long, String> createConsumer(){
+        //Construct standard Properties file
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                  BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                                  "KafkaExampleConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+              LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+              StringDeserializer.class.getName());
+
+      // Create the consumer using props.
+      final Consumer<Long, String> consumer =
+                                  new KafkaConsumer<>(props);
+
+      // Subscribe to the topic.
+      consumer.subscribe(Collections.singletonList(TOPIC));
+        return consumer;
+    }
+
+    static void runConsumer() throws InterruptedException {
+        final Consumer<Long, String> consumer = createConsumer();
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
+                        record.key(), record.value(),
+                        record.partition(), record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
+}  
--- src/main/deepProducer.java
+++ src/main/deepProducer.java
@@ -26,50 +26,74 @@
 package org.apache.kafka.clients;
 
 //Java imports declaration
-import java.util.*;
 import org.apache.kafka.clients.admin;
 import org.apache.kafka.clients.producer;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
+import java.util.Collections;
 
 //Create the Java class for the Kafka Topic Producer API
 public class deepProducer {
 
+    private final static String TOPIC = "swedatapipeline";
+    private final static String BOOTSTRAP_SERVERS =
+            "localhost:9092,localhost:9093,localhost:9094";
+            
     //Construct standard Properties file
-    Properties config = new Properties();
-    config.put("client.id", InetAddress.getLocalHost().getHostName());
-    config.put("bootstrap.servers", "host1:9092,host2:9092");
-    config.put("acks", "all");
-    new KafkaProducer<K,V>(config);
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                            BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                                        LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                                    StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+
     //Create the Kafka Topic Producer to send records
     //private static Producer<Long, String> createProducer(){
     //}
     //Main method to run the Producer API
-    static void runProducer(final int sendMessageCount) throws Exception {
-      final Producer<Long, String> producer = createProducer();
-      long time = System.currentTimeMillis();
-
-      try {
-          for (long index = time; index < time + sendMessageCount; index++) {
-              final ProducerRecord<Long, String> record =
-                      new ProducerRecord<>(TOPIC, index,
-                                  "Message Produced" + index);
-
-              RecordMetadata metadata = producer.send(record).get();
-
-              long elapsedTime = System.currentTimeMillis() - time;
-              System.out.printf("sent record(key=%s value=%s) " +
-                              "meta(partition=%d, offset=%d) time=%d\n",
-                      record.key(), record.value(), metadata.partition(),
-                      metadata.offset(), elapsedTime);
-
-          }
-      } finally {
-          producer.flush();
-          producer.close();
-      }
-  }
+    public static void main(String... args) throws Exception {
+        if (args.length == 0) {
+            runProducer(5);
+        } else {
+            runProducer(Integer.parseInt(args[0]));
+        }
+    }    
+    //runProducer function
+    static void runProducer(final int sendMessageCount) throws InterruptedException {
+        final Producer<Long, String> producer = createProducer();
+        long time = System.currentTimeMillis();
+        final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
+    
+        try {
+            for (long index = time; index < time + sendMessageCount; index++) {
+                final ProducerRecord<Long, String> record =
+                        new ProducerRecord<>(TOPIC, index, "Hello Mom " + index);
+                producer.send(record, (metadata, exception) -> {
+                    long elapsedTime = System.currentTimeMillis() - time;
+                    if (metadata != null) {
+                        System.out.printf("sent record(key=%s value=%s) " +
+                                        "meta(partition=%d, offset=%d) time=%d\n",
+                                record.key(), record.value(), metadata.partition(),
+                                metadata.offset(), elapsedTime);
+                    } else {
+                        exception.printStackTrace();
+                    }
+                    countDownLatch.countDown();
+                });
+            }
+            countDownLatch.await(25, TimeUnit.SECONDS);
+        }finally {
+            producer.flush();
+            producer.close();
+        }
+    }
+    
     //Send Kafka Message
     final ProducerRecord<K, V> record = new ProducerRecord<>(topic, key, value);
     Future<RecordMEtadata> future = producer.send(record);
